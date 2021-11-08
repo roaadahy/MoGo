84bce29267cd9b0957f1370514b046e41a2d07a4
Roaa Dahy <roaadahy1@gmail.com>
2021-11-08 16:24:36 +0200

    first

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+function changeToLTR(){
+    $('body').removeClass('direction-rtl').addClass('direction-ltr')
+  }
+  
+  function changeToRTL(){
+    $('body').removeClass('direction-ltr').addClass('direction-rtl');
+  }
